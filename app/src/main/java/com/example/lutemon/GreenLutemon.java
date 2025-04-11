416673d0af42064459e8e14bf6f97670e5645170
IroBlan <iro.blank@gmail.com>
2025-04-11 15:07:02 +0300

    my changes

--- app/src/main/java/com/example/lutemon/GreenLutemon.java
+++
@@ -1,7 +0,0 @@
-package com.example.lutemon;
-
-public class GreenLutemon extends Lutemon{
-    public GreenLutemon(String name, String color, int attack, int defence, int health, int maxHealth, int id,int experience){
-        super(name,"green", 6, 3, health, 19, id,0);
-    }
-}
