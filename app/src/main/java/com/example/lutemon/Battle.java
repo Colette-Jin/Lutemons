f9e42032ef5fdda4a1c56cbd8febe988c3bc1332
IroBlan <iro.blank@gmail.com>
2025-04-14 10:58:04 +0300

    BattleFieldFix

--- app/src/main/java/com/example/lutemon/Battle.java
+++ app/src/main/java/com/example/lutemon/Battle.java
@@ -3,5 +3,5 @@ import com.example.lutemon.mons.Lutemon;
 
 
 public interface Battle {
-    Lutemon fight(Lutemon lutemon1, Lutemon lutemon2);
+    void fight(Lutemon lutemon1, Lutemon lutemon2);
 }
--- app/src/main/java/com/example/lutemon/BattleField.java
+++ app/src/main/java/com/example/lutemon/BattleField.java
@@ -18,16 +18,19 @@ public class BattleField implements Battle {
 
     @Override
     public void fight(Lutemon A, Lutemon B) {
-        turn = 1
-        while true {
-            if rem(turn / 2) != 0 {//if the turn is odd A attacks
-                Lutemon atacker = A;
-                Lutemon defender = B;
+        int turn = 1;
+        Lutemon attacker;
+        Lutemon defender;
+
+        while (true) {
+            if (turn % 2 != 0) {//if the turn is odd A attacks
+                attacker = A;
+                defender = B;
             }else{//if the turn is even B attacks
-                Lutemon atacker = B;
-                Lutemon defender = A;
+                attacker = B;
+                defender = A;
             }
-            turn +=;// turn increases by one every loop iteration after attacker and defender are set
+            turn ++;// turn increases by one every loop iteration after attacker and defender are set
 
             attacker.attack(defender);//attacker attacks and defender defends
 
@@ -35,20 +38,19 @@ public class BattleField implements Battle {
             System.out.println(A.getName()+" has "+ A.getHealth()+" healthpoints");
             System.out.println(B.getName()+" has "+ B.getHealth()+" healthpoints");
 
-            if defender.getHealth() < 0 {
+            if (defender.getHealth() < 0) {
                 System.out.println(defender.getName()+" died. \n"+attacker.getName()+" wins.");
 
                 //return attacker home with new experience
-                attacker.addExperice(1);
-                BattleField.moveToHome(attacker);
+                attacker.addExperience(1);
+                this.moveToHome(attacker);
 
                 //return defender home with its stats put back to the default
                 defender.resetAllParametersToDefault();
-                BattleField.moveToHome(defender);
-
+                this.moveToHome(defender);
                 break;//end the loop
             }else{
-                System.out.println(defender.getName()+"managed to excape death.");
+                System.out.println(defender.getName()+"managed to escape death.");
                 // loop continues
             }
         }
